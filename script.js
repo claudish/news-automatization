b486d3c1b3f0a87a1c285d9979c822c9260adbf5
claudish <57326763+claudish@users.noreply.github.com>
2020-08-10 21:11:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function parseWordDocxFile(inputElement) {
 
 
                 for (var h2 of div.getElementsByTagName("p")) {
-                    if (h2.textContent.includes("h2 ")) {
+                    if (h2.textContent.includes("h2:")) {
                         var header = document.createElement("h2");
                         header.textContent = h2.textContent.substr(4);
                         h2.replaceWith(header);
@@ -64,31 +64,20 @@ function parseWordDocxFile(inputElement) {
                     h.id = i++;
                 })
 
-
+                
+            var description = div.getElementsByTagName("p"); 
+          if (description.length > 0) {
+         description[0].remove(); }
+    
+          
+  
 
                 document.getElementById("spistresci").innerHTML += div.innerHTML;
                 document.getElementById("spistresci").innerHTML += "\n" + `<div class="text-center"><a href="#" class="btn btn-primary btn-lg">Sprawdź</a></div>`;
-             document.getElementById("spistresci").innerHTML += "\n" + `<div class="similar-news">
+                document.getElementById("spistresci").innerHTML += "\n" + `<div class="similar-news">
         <div class="news-text-title">Polecane artykuły</div>
         <div class="simnews-items" data-news-ids="7909,8164,8119">
-            <!-- 1 -->
-            <a class="simnews-item-link"
-               href="https://www.morele.net/wiadomosc/jak-dbac-o-bezpieczenstwo-komputera-i-danych/7909/"
-               title="Jak dbać o bezpieczeństwo komputera i danych">
-                Jak dbać o bezpieczeństwo komputera i danych
-            </a>
-            <!-- 2 -->
-            <a class="simnews-item-link"
-               href="https://www.morele.net/wiadomosc/skroty-klawiszowe-windows-ktore-ulatwia-ci-zycie/8164/"
-               title="Skróty klawiszowe Windows, które ułatwią Ci życie!">
-                Skróty klawiszowe Windows, które ułatwią Ci życie!
-            </a>
-            <!-- 3 -->
-            <a class="simnews-item-link"
-               href="https://www.morele.net/wiadomosc/laptop-wolno-chodzi-i-wciaz-sie-zawiesza/8119/"
-               title="Laptop wolno chodzi i wciąż się zawiesza. Jak to naprawić?">
-                Laptop wolno chodzi i wciąż się zawiesza. Jak to naprawić?
-            </a>
+
             <div class="w-100 d-flex my-5">
                 <span class="circle-loading mx-auto"></span>
             </div>
