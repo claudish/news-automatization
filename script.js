a23b2ab02ef329dc2bbf9dd930143b703309dd96
claudish <57326763+claudish@users.noreply.github.com>
2021-06-13 21:35:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function prepareAnhorHref(title) {
+ return title
+   .normalize("NFD")
+   .replace(/\p{Diacritic}/gu, "") // remove accents
+   .toLowerCase()
+   .replace(/[^a-zA-Z0-9 ]/g, "") // remove special chars
+   .replace(/ +(?= )/g,'') // remove double spaces
+   .replace(/ /g, '_'); // spaces to under_score
+}
+
 function parseWordDocxFile(inputElement) {
     var files = inputElement.files || []
     if (!files.length) return
@@ -63,15 +73,14 @@ function parseWordDocxFile(inputElement) {
                 headers.forEach((header, i) => {
                     var listViewItem = document.createElement('li');
                     var a = document.createElement('a');
-                    a.href = `#${++i}`;
+                    a.href = '#' + prepareAnhorHref(header.innerHTML);
                     a.title = header.innerHTML;
                     a.innerHTML = header.innerHTML;
                     listViewItem.appendChild(a);
                     document.getElementsByTagName("ol")[0].appendChild(listViewItem);
                     var h = document.createElement('h2');
                     var p = document.createElement('p');
-                    header.id = i;
-                    h.id = i++;
+                    header.id = prepareAnhorHref(header.innerHTML);
                 })
 
                 
@@ -85,7 +94,6 @@ function parseWordDocxFile(inputElement) {
                 document.getElementById("spistresci").innerHTML += "\n" + `<div class="similar-news">
         <div class="news-text-title">Polecane artykuły</div>
         <div class="simnews-items" data-news-ids="7909,8164,8119">
-
             <div class="w-100 d-flex my-5">
                 <span class="circle-loading mx-auto"></span>
             </div>
